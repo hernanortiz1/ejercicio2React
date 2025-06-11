20ed407df44674e0cda1f4adc3de0e022e4b1040
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-10 23:47:11 -0300

    agregue componente HelloWord y envie string con props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import HelloWord from "./components/HelloWorld";
+
 function App() {
- 
+  const mensajeProps = "my friend!";
   return (
     <>
-      
+      <HelloWord mensajeProps={mensajeProps} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
